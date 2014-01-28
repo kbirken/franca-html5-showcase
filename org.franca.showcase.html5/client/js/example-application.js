7408b5f377c1ce956f04909f5c9a7292115247b7
Klaus Birken <klaus.birken@gmail.com>
2014-01-28 12:49:57 +0100

    Initial version of Infotainment showcase for Franca’s websocket generator.

---
+++ org.franca.showcase.html5/client/js/example-application.js
@@ -0,0 +1,127 @@
+
+// switch page programmatically: $.mobile.changePage("#pNav")
+
+function initApp() {
+	// initialize tacho widget	
+	var tacho = Raphael("tacho", 260, 260).tachometer(0, {
+		interactive: true,
+		needleAnimationEasing: "linear", // or: <>, bounce, elastic
+		needleAnimationDuration: 500,
+		numberMax: 160,
+		longScaleCount: 8,
+		shortScaleCount: 80,
+		numberUnit: " km/h"
+	});
+	
+	// this fixes a tachometer layout issue on WebKit (Chrome, Safari)
+	var elems = $('tspan');
+	for(var i = 0; i < elems.length; i++) {
+		elems[i].setAttribute('dy', 0);
+	}
+
+	// initialize proxy for SimpleUI interface
+	var proxy = new SimpleUIProxy();
+	proxy.connect('ws://localhost:8181');
+
+	// register callback for SimpleUI.onChangedClock() updates
+	proxy.onChangedClock = function(clock) {
+		$('#tClock').text(clock);
+	};
+
+	// register callback for SimpleUI.playingTitle() broadcast
+	proxy.signalPlayingTitle = function(title) {
+		document.getElementById('current-title').innerHTML = title;
+	};
+
+	// register callback for SimpleUI.updateVelocity() broadcast
+	proxy.signalUpdateVelocity = function(velocity) {
+		tacho.set(velocity);
+	};
+	
+	proxy.onOpened = function() {
+		console.log('The connection has been opened!')
+		proxy.subscribeClockChanged();
+	}
+	
+	proxy.onClosed = function() {
+		console.log('The connection has been closed!')
+	}
+
+	// connect UI buttons with playMusic() calls
+	$("#m1").click(function() { proxy.playMusic(Genre.M_NONE); });
+	$("#m2").click(function() { proxy.playMusic(Genre.M_POP); });
+	$("#m3").click(function() { proxy.playMusic(Genre.M_TECHNO); });
+	$("#m4").click(function() { proxy.playMusic(Genre.M_TRANCE); });
+
+
+	$(document).on( "pageinit", "#pNav", function() {
+		$("#mStart").click(function() {
+			proxy.startNavigation(
+				document.getElementById("street").value,
+				document.getElementById("city").value);
+		});
+
+		// register callback for SimpleUI.startNavigation() replies
+		proxy.replyStartNavigation = function(cid, routeLength) {
+			$('#navresult').text("Distance to destination: " + routeLength + " km");
+		};
+
+	});
+
+	/*
+	 * Google Maps documentation: http://code.google.com/apis/maps/documentation/javascript/basics.html
+	 * Geolocation documentation: http://dev.w3.org/geo/api/spec-source.html
+	 */
+	$(document).on( "pageinit", "#pMap", function() {
+		var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
+		if ( navigator.geolocation ) {
+			function success(pos) {
+				// Location found, show map with these coordinates
+				drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+			}
+			function fail(error) {
+				drawMap(defaultLatLng);  // Failed to find location, show default map
+			}
+			// Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
+			navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+		} else {
+			drawMap(defaultLatLng);  // No geolocation support, show default map
+		}
+
+		function drawMap(latlng) {
+			var myOptions = {
+				zoom: 10,
+				center: latlng,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+
+			// Add an overlay to the map of current lat/lng
+			var marker = new google.maps.Marker({
+				position: latlng,
+				map: map,
+				title: "You are here!"
+			});
+		}
+	});
+}
+
+
+function initFixedHeaders() {
+	jQuery.fn.headerOnAllPages = function() {
+		var theHeader = $('#constantheader-wrapper').html();
+			var allPages = $('div[pagetype="standard"]');
+
+		for (var i = 1; i < allPages.length; i++) {
+			allPages[i].innerHTML = theHeader + allPages[i].innerHTML;
+			}
+	};
+
+	$(function() {
+		$().headerOnAllPages();
+		$( "[data-role='navbar']" ).navbar();
+		$( "[data-role='header'], [data-role='footer']" ).toolbar();
+	});
+
+
+}
---
+++ org.franca.showcase.html5/server_nodejs/index.js
@@ -0,0 +1,80 @@
+// create http server and listen to port 8080
+// we need this to serve index.html and other files to the client
+var HttpServer = require('./base/util/HttpServer');
+var http = new HttpServer();
+http.init(8180, '../client');
+
+// MusicPlayer application
+var MusicPlayer = require('./applications/MusicPlayer');
+var player = new MusicPlayer();
+
+// Vehicle application
+var Vehicle = require('./applications/Vehicle');
+var vehicle = new Vehicle();
+vehicle.init();
+
+// create websocket stub for SimpleUI interface and listen to websocket port.
+var SimpleUIStub = require('./gen/org/example/SimpleUIStub');
+var stub = new SimpleUIStub(8181);
+
+// set initial values for attributes
+stub.clock = getTime();
+
+stub.init();
+
+stub.onClientConnected = function(clientID) {
+	console.log('The ID of the newly connected client is ' + clientID);
+};
+
+stub.onClientDisconnected = function(clientID) {
+	console.log('The client with ID ' + clientID + ' has disconnected');
+}
+
+stub.playMusic = function (genre) {
+	var d = "";
+	
+	switch (genre) {
+		case SimpleUIStub.Genre.M_NONE:   player.stop(); break;
+		case SimpleUIStub.Genre.M_POP:    player.play('http://icecast.radio24.ch/radio24pop'); break;
+		case SimpleUIStub.Genre.M_TECHNO: player.play('http://firewall.pulsradio.com'); break;
+		case SimpleUIStub.Genre.M_TRANCE: player.play('http://firewall.trance.pulsradio.com'); break;
+		default: console.error("Invalid value " + genre + " for parameter 'genre'!");
+	}
+}
+
+player.onStreamTitle = function(title) {
+	// forward to UI
+	stub.playingTitle(title);
+}
+
+vehicle.onUpdateVelocity = function(vel) {
+    // send velocity broadcast
+    stub.updateVelocity(vel);
+}
+
+stub.startNavigation = function (street, city) {
+	console.log("startNavigation: street=" + street + " city=" + city);
+	return {"routeLength" : street.length + 10*city.length, "arrivalTime" : "22:00"};
+};
+
+
+var driveTimerID = setInterval(function() {
+	stub.setClock(getTime());
+}, 1000);
+
+function getTime() {
+	var date = new Date();
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    return hour + ":" + min + ":" + sec
+}
+
+
+
